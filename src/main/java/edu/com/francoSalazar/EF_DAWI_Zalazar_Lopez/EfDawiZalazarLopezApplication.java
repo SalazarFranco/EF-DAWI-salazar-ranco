9adb2bd3c9b69ab3caf776255211411deca90c70
SalazarFranco <francosalazar057@gmail.com>
2024-12-17 15:58:24 -0500

    proyecto creado

---
+++ src/main/java/edu/com/francoSalazar/EF_DAWI_Zalazar_Lopez/EfDawiZalazarLopezApplication.java
@@ -0,0 +1,13 @@
+package edu.com.francoSalazar.EF_DAWI_Zalazar_Lopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfDawiZalazarLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfDawiZalazarLopezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/com/francoSalazar/EF_DAWI_Zalazar_Lopez/EfDawiZalazarLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.francoSalazar.EF_DAWI_Zalazar_Lopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfDawiZalazarLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
